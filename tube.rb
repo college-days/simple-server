774d43cd6df0f22f70be801826cad2c3a1786ebf
zjhmale <zjhmale@gmail.com>
2014-07-14 15:50:48 +0800

    split single function into parts and add the loop to receive requests

--- tube.rb
+++ tube.rb
@@ -4,19 +4,39 @@ class Tube
   def initialize(port)
     @server = TCPServer.new(port)
   end
-
+  
   def start
-    socket = @server.accept
-    data = socket.readpartial(1024)
-    puts data
+    loop do
+      socket = @server.accept
+      connection = Connection.new(socket)
+      connection.process
+    end  
+  end
 
-    socket.write "HTTP/1.1 200 OK \r\n"
-    socket.write "\r\n"
-    #or just this also works fine
-    #socket.write "HTTP/1.1 200 OK \n"
-    socket.write "hello\n"
+  class Connection
+    def initialize(socket)
+      @socket = socket
+    end
 
-    socket.close
+    def process
+      data = @socket.readpartial(1024)
+      puts data
+      self.send_response
+    end
+
+    def send_response
+      @socket.write "HTTP/1.1 200 OK \r\n"
+      @socket.write "\r\n"
+      # just follow the ruby code style guide and the #@ beside problem we use #<whitespace> to comment a single line
+      # or just this also works fine
+      # @socket.write "HTTP/1.1 200 OK \n"
+      @socket.write "hello\n"
+      self.close
+    end
+
+    def close
+      @socket.close
+    end
   end
 end
 
